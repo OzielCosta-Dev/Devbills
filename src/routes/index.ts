6b1f67e5115af062ec309767b3a5b4bad07479d4
OzielCosta-Dev <oziel19932016@outlook.com>
2026-07-20 15:05:50 -0300

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import type { FastifyInstance } from "fastify"
+
+async function routes(fastify: FastifyInstance): Promise<void>{
+
+    fastify.get('/health', async() => {
+        return {
+            status: 'Ok',
+            message: 'DevBills API rodando normalmente'
+        }
+    })
+}
+
+export default routes
