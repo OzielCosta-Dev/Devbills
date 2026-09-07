1b33a01920680cc3def022529dd136599cb585c8
OzielCosta-Dev <oziel19932016@outlook.com>
2026-09-07 17:53:27 -0300

    realizado-testes-no-insominia

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,22 @@ import app from './app.js';
 import { prismaConnect } from './config/prisma.js';
 import { initializeGlobalCategories } from './services/gloabalCategories.service.js';
 import { env } from './config/env.js';
+import initializeFirebaseAdmin from "./config/firebase.js";
 
 const PORT = env.PORT;
 
 const startServer = async () => {
 
     try{
+
+        initializeFirebaseAdmin();
         await prismaConnect();
 
         await initializeGlobalCategories();
 
         await app.listen({ port: PORT}).then( () => {
             console.log(`Servidor rodando na porta ${PORT}`)
+            console.log("🔥 Firebase Admin inicializado");
         })
 
     } catch(err){
