3b78cb8a40e7da1ed3fd97b9c28fcbc1035e34e0
OzielCosta-Dev <oziel19932016@outlook.com>
2026-09-23 22:43:31 -0300

    implementando-o-gethistoricaltransactions

--- src/controllers/transactions/getHistoricalTransactions.controler.ts
+++ src/controllers/transactions/getHistoricalTransactions.controler.ts
@@ -1,7 +1,51 @@
-import type { FastifyRequest } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 import type { GetHistoricalTransactionsQuery } from "../../schemas/transactions.schema.js";
+import dayjs from "dayjs";
+import prisma from "../../config/prisma.js";
+import { lessThanOrEqual } from "firebase/firestore/pipelines";
 
 export const getHistoricalTransactions = async (
     request: FastifyRequest<{Querystring: GetHistoricalTransactionsQuery}>,
-    reply: FastfyReply
-)
+    reply: FastifyReply
+): Promise<void> => {
+        const userId = request.userId; 
+
+       if(!userId){
+         reply.status(401).send({ error: "Usuário não autenticado"})
+         return;
+      }
+
+    const { month, year, months = 6 } = request.query;
+
+
+    const baseDate = new Date(year, month -1, 1)
+
+    const startDate = dayjs(baseDate).subtract( months -1, 'month').startOf('month').toDate();
+    const endDate = dayjs(baseDate).endOf('month').toDate();
+
+
+    try {
+        
+
+         const transactions = await prisma.transaction.deleteMany({
+            where: {
+                userId,
+                date: {
+                    gte: startDate, 
+                    lte: endDate,
+                }
+
+            }, 
+            select: {
+                amount: true,
+                type: true,
+                date: true,
+            }
+         })
+
+
+    } catch (err) {
+        
+    }
+  
+};
